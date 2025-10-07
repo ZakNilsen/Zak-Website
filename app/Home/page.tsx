6bac299cf46f0a4ae1dbc6de48f020977cb2a4f5
ZakNilsen <zakarynilsen@gmail.com>
2025-10-07 15:40:57 -0500

    More Setup

---
+++ app/Home/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div className={styles.ctas}>
+          
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        Footer
+      </footer>
+    </div>
+  );
+}
